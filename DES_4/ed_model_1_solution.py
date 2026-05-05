5bf1e11603d7bf1b0918a92a24c01539aa12b972
hsma-chief-elf <d.chalk@exeter.ac.uk>
2026-05-05 10:29:04 +0100

    ed_model_1_solution.py: added Patient class

--- DES_4/ed_model_1_solution.py
+++ DES_4/ed_model_1_solution.py
@@ -7,3 +7,12 @@ import numpy as np
 import plotly.express as px
 import plotly.graph_objects as go
 
+class Patient:
+    def __init(self, p_id):
+        self.id = p_id
+
+        self.q_time_reg = pd.NA
+        self.q_time_triage = pd.NA
+        self.q_time_treat = pd.NA
+        self.q_time_pharmacy = pd.NA
+
