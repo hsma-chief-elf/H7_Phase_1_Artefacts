7afaafb1d5928d3cefadc7bc96d2c3ef52bf2c22
hsma-chief-elf <d.chalk@exeter.ac.uk>
2026-05-05 12:39:48 +0100

    simpy_warm_up_analysis.py: added code to set warm up period to 0 when running warm up analysis using run_warm_up_assessment() method of Model class

--- DES_4/simpy_warm_up_analysis.py
+++ DES_4/simpy_warm_up_analysis.py
@@ -125,6 +125,7 @@ class Model:
 
     # NEW
     def run_warm_up_assessment(self):
+        self.param.warm_up_period = 0
         self.env.process(self.generator_patient_arrivals())
         self.env.process(self.cumulative_mean_tracker())
         self.env.run(until=self.param.sim_duration_warm_up_assessment)
