682ad823a5444e51864861d5bb61e205811dcebb
hsma-chief-elf <d.chalk@exeter.ac.uk>
2026-05-05 14:03:43 +0100

    ed_model_1_solution.py: added run_trial() method

--- DES_4/ed_model_1_solution.py
+++ DES_4/ed_model_1_solution.py
@@ -308,4 +308,14 @@ class Trial:
         self.se_q_time_pharm = pd.NA
         self.warm_up_trial = False
 
+    def run_trial(self):
+        for replication_id in range(self.param.num_replications):
+            model_replication = Model(self.param, replication_id)
+            model_replication.run_model()
+            patient_df = model_replication.convert_entity_list_to_dataframe(
+                model_replication.list_of_patients
+            )
+            model_replication.calculate_run_results(patient_df)
+            self.list_of_simulation_replications.append(model_replication)
+
     
