596954f7d121b43dc8e7e05e965e923f1cf935b5
hsma-chief-elf <d.chalk@exeter.ac.uk>
2026-07-06 16:00:39 +0100

    governer_model.py: added Patient class

--- DES_5/governor_model.py
+++ DES_5/governor_model.py
@@ -1,1 +1,18 @@
 import simpy
+from sim_tools.distributions import Lognormal
+import pandas as pd
+import math
+from scipy import stats
+import numpy as np
+import plotly.express as px
+import plotly.graph_objects as go
+import os
+from pathlib import Path
+
+os.chdir(Path(__file__).parent)
+
+class Patient:
+    def __init__(self, p_id):
+        self.id = p_id
+        self.num_follow_ups = pd.NA
+
