6ea1257efe1143f85a54dc31c52d8702e180955d
hsma-chief-elf <d.chalk@exeter.ac.uk>
2026-07-07 10:53:56 +0100

    governor_model.py: wrote Model constructor and added mean interarrival to Param class

--- DES_5/governor_model.py
+++ DES_5/governor_model.py
@@ -20,6 +20,7 @@ class Param:
     def __init__(
         self,
         num_slots_per_day = 10,
+        mean_referral_inter = 0.1,
         results_collection_period = 365,
         warm_up_period = 365,
         num_replications = 100,
@@ -28,6 +29,7 @@ class Param:
         cumulative_mean_tracker_interval = 1
     ):
         self.num_slots_per_day = num_slots_per_day
+        self.mean_referral_inter = mean_referral_inter
         self.results_collection_period = results_collection_period
         self.warm_up_period = warm_up_period
         self.sim_duration = warm_up_period + results_collection_period
@@ -41,4 +43,27 @@ class Param:
         self.cumulative_mean_tracker_interval = (
             cumulative_mean_tracker_interval
         )
+
+class Model:
+    def __init__(self, param, replication_id):
+        self.param = param
+        self.replication_id = replication_id
+        self.env = simpy.Environment()
+        self.patient_counter = 0
+
+        self.daily_slots = simpy.Container(
+            env,
+            self.param.num_slots_per_day,
+            init=self.param.num_slots_per_day
+        )
+
+        ss = np.random.SeedSequence(self.replication_id)
+        seeds = ss.spawn(1)
+
+        self.referral_inter_dist = Exponential(
+            mean=self.param.mean_referral_inter,
+            random_seed=seeds[0]
+        )
+
+        self.list_of_patients = []
 
