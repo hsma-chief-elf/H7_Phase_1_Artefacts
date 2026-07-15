c7c711e1243f2e83f14f0261516e81f7dbd3599d
hsma-chief-elf <d.chalk@exeter.ac.uk>
2026-07-15 12:31:03 +0100

    community_msk_solution.py: added Param class

--- DES_5/community_msk_solution.py
+++ DES_5/community_msk_solution.py
@@ -19,3 +19,26 @@ class Patient:
         self.q_time_physio_apts = {}
         self.q_time_injection_apts = {}
 
+class Param:
+    def __init__(
+        self,
+        transition_prob_matrix_csv,
+        num_assessment_slots_per_day = 10,
+        num_physio_slots_per_day = 10,
+        num_injection_slots_per_day = 10,
+        mean_referrals_per_day = 10.0,
+        results_collection_period = (365) * 5,
+        warm_up_period = 365,
+        num_replications = 100
+    ):
+        self.transition_prob_matrix_df = (
+            pd.read_csv(transition_prob_matrix_csv)
+        )
+        self.num_assessment_slots_per_day = num_assessment_slots_per_day
+        self.num_physio_slots_per_day = num_physio_slots_per_day
+        self.num_injection_slots_per_day = num_injection_slots_per_day
+        self.mean_referrals_per_day = mean_referrals_per_day
+        self.results_collection_period = results_collection_period
+        self.warm_up_period = warm_up_period
+        self.num_replications = num_replications
+
